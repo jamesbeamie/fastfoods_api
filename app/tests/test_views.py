8f60ef2343b1a60d2d0434d9f1287e20b2b2c9d6
jamesbeamie <wafulajames9@gmail.com>
2018-09-21 00:05:58 +0300

    [finishes #160309461]

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -11,13 +11,22 @@ class TestApi(unittest.TestCase):
   """The class with individual tests for individual endpoints"""
   
   def setUp(self):
-  	self.app = create_app(config_name="testing")
-  	self.client = self.app.test_client
-  	
+    self.app = create_app(config_name="testing")
+    self.client = self.app.test_client
+    
   def test_return_all(self):
       """Test if all orders are returned and returns success code,200"""
       response = self.client().get('/api/v1/orders', content_type='application/json')
       self.assertEqual( response.status_code, 200)
+
+  def test_return_specific(self):
+    """Test if specified order is returned and returns success code,200"""
+    #test if the order id is not specified, returns page not found,404
+    response = self.client().get('/api/v1/orders/1', content_type='application/json')
+    self.assertEqual( response.status_code, 200)
+
+    response = self.client().get('/api/v1/orders/', content_type='application/json')
+    self.assertEqual( response.status_code, 404)
     
 if __name__ == "__main__":
-	unittest.main()
+  unittest.main()
--- app/v1/orders/views.py
+++ app/v1/orders/views.py
@@ -9,3 +9,11 @@ def all_orders():
   """ Method to place and get Orders."""
   available_orders = order_class.all_order()
   return available_orders
+
+@api.route('/orders/<int:order_id>', methods=['GET'])
+def specific(order_id, **kwargs):
+	"""method to return a specific order"""
+	result = order_class.return_specific(order_id)
+	if not result:
+		return jsonify({"message":"couldn't find order_id"})
+	return result
