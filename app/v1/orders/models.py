28918d93c5efb33950f6d44acad647f946a4286a
jamesbeamie <wafulajames9@gmail.com>
2018-09-21 07:32:51 +0300

    implemented OOP

--- app/v1/orders/models.py
+++ app/v1/orders/models.py
@@ -2,28 +2,10 @@ from flask import jsonify, request
 class Orders(object):
     def __init__(self):
         """ Initialize empty order list"""  
-        self.all_orders = []
+        self.all_orders = []        
 
     def all_order(self):
         """ fetch all orders """
         if len(self.all_orders) > 0:
             return jsonify({"Orders": self.all_orders}), 200
-        return jsonify({"message":"Kindly place your order."})        
-
-    def return_specific(self, order_id):
-        """The function returns a specific order, specified by id"""
-        for order in self.all_orders:
-            if order['order_id'] == order_id:
-                return jsonify({"order":order})
-
-    def place_order(self, name, price):
-        """Create order"""
-        self.order = {}
-        self.order_id = len(self.all_orders)
-
-        self.order['order_id'] = self.order_id + 1
-        self.order['name'] = name
-        self.order['price'] = price
-
-        self.all_orders.append(self.order)
-        return jsonify({"message": "Order placed.", "Orders":self.all_orders}), 201
+        return jsonify({"message":"Kindly place your order."})
--- app/v1/orders/views.py
+++ app/v1/orders/views.py
@@ -3,25 +3,12 @@ from . import api
 from .models import Orders
 
 order_class = Orders()
-
-@api.route('/orders', methods=["GET"])
-def all_orders():
-  """ Method to place and get Orders."""
-  available_orders = order_class.all_order()
-  return available_orders
-
-@api.route('/orders/<int:order_id>', methods=['GET'])
-def specific(order_id, **kwargs):
-	"""method to return a specific order"""
-	result = order_class.return_specific(order_id)
-	if not result:
-		return jsonify({"message":"couldn't find order_id"})
-	return result
-
-@api.route('/orders', methods=['POST'])
-def place_order():
-	data = request.get_json()
-	name = data['name']
-	price = data['price']
-	res = order_class.place_order(name, price)
-	return res
+"""
+orders
+"""
+class OrdersViews():
+	@api.route('/orders', methods=["GET"])
+	def all_orders():
+	  """ Method to place and get Orders."""
+	  available_orders = order_class.all_order()
+	  return available_orders
