fdd85dc73d9292113fc7d149db19952ba56fd3ea
jamesbeamie <wafulajames9@gmail.com>
2018-09-21 08:06:28 +0300

    Updated end point to return specific order to OOP

--- app/v1/orders/views.py
+++ app/v1/orders/views.py
@@ -3,17 +3,20 @@ from . import api
 from .models import Orders
 
 order_class = Orders()
+"""
+orders
+"""
+class OrdersViews():
+	@api.route('/orders', methods=["GET"])
+	def all_orders():
+	  """ Method to place and get Orders."""
+	  available_orders = order_class.all_order()
+	  return available_orders
 
-@api.route('/orders', methods=["GET"])
-def all_orders():
-  """ Method to place and get Orders."""
-  available_orders = order_class.all_order()
-  return available_orders
-
-@api.route('/orders/<int:order_id>', methods=['GET'])
-def specific(order_id, **kwargs):
-	"""method to return a specific order"""
-	result = order_class.return_specific(order_id)
-	if not result:
-		return jsonify({"message":"couldn't find order_id"})
-	return result
+	@api.route('/orders/<int:order_id>', methods=['GET'])
+	def specific(order_id, **kwargs):
+		"""method to return a specific order"""
+		result = order_class.return_specific(order_id)
+		if not result:
+			return jsonify({"message":"couldn't find order_id"})
+		return result
