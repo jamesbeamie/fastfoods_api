bf6c5afa53de1f0f882199b0cf33427622e52d50
jamesbeamie <wafulajames9@gmail.com>
2018-09-26 21:08:31 +0300

    [finishes #160711027] added an endpoint to place an order and insert into the database

--- app/v2/orders/models.py
+++ app/v2/orders/models.py
@@ -5,7 +5,7 @@ from ...dbconect import dbcon
 class Orders(object):
     def __init__(self):
         """ Initialize empty order list"""  
-        self.all_orders = []        
+        #self.all_orders = []        
 
     def all_order(self):
         """ fetch all orders """
@@ -15,3 +15,13 @@ class Orders(object):
         res = cur.fetchall()
         return jsonify({"message":"retrieved", "Orders": res}), 200
 
+    def place_order(self, food_name, price, food_id, order_status):
+        """Create order"""
+        con = dbcon()
+        cur = con.cursor()
+        cur.execute("INSERT INTO myorders (food_name, price, food_id, order_status) VALUES (%(food_name)s,%(price)s,%(food_id)s,%(order_status)s);",{'food_name':food_name,'price':price,'food_id':food_id,'order_status':order_status})
+        con.commit()
+        return jsonify({"message":"order created"}), 200
+
+        #self.all_orders.append(self.order)
+        #return jsonify({"message": "Order placed.", "Orders":self.all_orders}), 201
--- app/v2/orders/views.py
+++ app/v2/orders/views.py
@@ -11,4 +11,12 @@ class OrdersViews():
 	def all_orders():
 		""" Method to place and get Orders."""
 		return order_class.all_order()
-
+  
+	@api2.route('/orders', methods=['POST'])
+	def place():
+		data = request.get_json()
+		food_name = data['food_name']
+		price = data['price']
+		food_id = data['food_id']
+		order_status = data['order_status']
+		return order_class.place_order(food_name, price, food_id, order_status)
