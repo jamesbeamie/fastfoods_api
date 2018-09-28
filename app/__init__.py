2d4f94ac3e05028b6ef7a80e287b79e1622c345c
james <42789760+jamesbeamie@users.noreply.github.com>
2018-09-28 21:21:49 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,20 +4,16 @@ from flask import Flask
 
 # local imports
 from config import app_config
-from .dbconect import init_db
+
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
 
-    #initialize database 
-    init_db()
 
     from .v1.orders import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
-    from .v2.orders import api2 as api2_blueprint
-    app.register_blueprint(api2_blueprint, url_prefix='/api/v2')
     
     return app
   
