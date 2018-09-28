697b9c502fa1d7b9409571df67c9754bfde5b21b
james <42789760+jamesbeamie@users.noreply.github.com>
2018-09-28 21:47:40 +0300

    Update test_views.py

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,15 +1,16 @@
 """The module has test for the api"""
-from flask import session
 import unittest
 import os
 import json
+#from flask_testing import TestCase
 from app import create_app
-
+"""the dictionaries are used for testing endpoints"""
 #class for the unittests
+
 class TestApi(unittest.TestCase):
   """The class with individual tests for individual endpoints"""
   def setUp(self):
-    """Setting up test client"""
+    self.app = create_app(config_name="testing")
     self.client = self.app.test_client
 
 class TestOrder(TestApi):
