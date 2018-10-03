039ed2e872fc72b1761f5db11146798290069514
jamesbeamie <wafulajames9@gmail.com>
2018-10-03 14:54:59 +0300

    [finishes #160953552] Put environment variables in .env file to be exported

--- app/__init__.py
+++ app/__init__.py
@@ -11,16 +11,15 @@ from config import app_config
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.config['JWT_SECRET_KEY']= 'mysecretkey'
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=5)
+    mykey = os.getenv('SECRET')
+    app.config['JWT_SECRET_KEY']= mykey
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=50)
     #initialize jwt manager
     jwt = JWTManager(app)
     app.config.from_object(app_config[config_name])
     #initialize db
     init_db()
 
-    from .v1.endpoints import api as api_blueprint
-    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
     from .v2.endpoints import api2 as api2_blueprint
     app.register_blueprint(api2_blueprint, url_prefix='/api/v2')
     
